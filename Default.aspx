3f8a003a56da0805ab797aeaf5dad323c22de468
Krunal Bhadresha <krunalbhadresa@gmail.com>
2018-03-04 01:14:41 -0500

    Add files via upload

---
+++ Default.aspx
@@ -0,0 +1,43 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="Default.aspx.cs" Inherits="_Default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+    
+        <asp:Label ID="Label1" runat="server" Text="BLACK JACK GAME"></asp:Label>
+    
+    </div>
+        <p>
+            <asp:Label ID="Label2" runat="server" Text="Cards    "></asp:Label>
+            <asp:TextBox ID="c1" runat="server" Width="50px"></asp:TextBox>
+            <asp:TextBox ID="c2" runat="server" Width="50px"></asp:TextBox>
+        </p>
+        <p>
+            <asp:Label ID="Label3" runat="server" Text="New card "></asp:Label>
+            <asp:TextBox ID="hc" runat="server" Width="131px"></asp:TextBox>
+        </p>
+        <p>
+            <asp:Label ID="utotal" runat="server"></asp:Label>
+            <asp:Label ID="uscore" runat="server"></asp:Label>
+        </p>
+        <asp:Button ID="deal" runat="server" OnClick="deal_Click" Text="Deal" />
+        <asp:Button ID="hit" runat="server" OnClick="hit_Click" Text="Hit" />
+        <asp:Button ID="pass" runat="server" OnClick="pass_Click" Text="Pass" />
+        <p>
+            <asp:Label ID="dtotal" runat="server"></asp:Label>
+            <asp:Label ID="dscore" runat="server"></asp:Label>
+        </p>
+        <asp:Label ID="result" runat="server"></asp:Label>
+        <p>
+            <asp:Button ID="restart" runat="server" OnClick="restart_Click" Text="Play again" />
+            <asp:Button ID="exit" runat="server" Text="Exit" />
+        </p>
+    </form>
+</body>
+</html>
---
+++ Default.aspx.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class _Default : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        
+    }
+
+    protected void deal_Click(object sender, EventArgs e)
+    {
+        Random rnd = new Random();
+
+        int card1 = rnd.Next(1,11);
+        c1.Text = Convert.ToString(card1);
+
+        int card2 = rnd.Next(1, 11);
+        c2.Text = Convert.ToString(card2);
+
+        utotal.Text = "Total :";
+        uscore.Text = Convert.ToString(card1 + card2);
+
+        deal.Enabled = false;
+        
+    }
+
+    protected void hit_Click(object sender, EventArgs e)
+    {
+        Random rnd = new Random();
+        int hcard = rnd.Next(1, 11);
+        hc.Text = Convert.ToString(hcard);
+
+        uscore.Text = Convert.ToString(Convert.ToInt32(uscore.Text) + Convert.ToInt32(hc.Text)) ;
+        if(Convert.ToInt32(uscore.Text) > 21)
+        {
+            result.Text = "Sorry, You lost !!";
+            deal.Enabled = false;
+            hit.Enabled = false;
+            pass.Enabled = false;
+        }
+    }
+
+
+    protected void pass_Click(object sender, EventArgs e)
+    {
+        Random rnd = new Random();
+        int d1 = rnd.Next(1, 11);
+        int d2 = rnd.Next(1, 11);
+        dscore.Text = Convert.ToString(d1 + d2);
+        dtotal.Text = "Dealer's score :";
+
+        do
+        {
+            int d3 = rnd.Next(1, 11);
+            dscore.Text = Convert.ToString(Convert.ToInt32(dscore.Text) + d3);
+        } while (Convert.ToInt32(dscore.Text) < 17);
+
+        if (Convert.ToInt32(dscore.Text) >= 17 && Convert.ToInt32(dscore.Text) < 21)
+        {
+            if (Convert.ToInt32(uscore.Text) >= Convert.ToInt32(dscore.Text))
+            {
+                result.Text = "Congratulations, You won !!";
+            }
+            else
+            {
+                result.Text = "Sorry, You lost !!";
+            }
+        }
+        else
+        {
+            result.Text = "Congratulations, You won !!";
+        }
+        hit.Enabled = false;
+        pass.Enabled = false;
+
+    }
+
+    protected void restart_Click(object sender, EventArgs e)
+    {
+        Page_Load(sender, e);
+        c1.Text = "";
+        c2.Text = "";
+        hc.Text = "";
+        utotal.Text = "";
+        uscore.Text = "";
+        dtotal.Text = "";
+        dscore.Text = "";
+        result.Text = "";
+        deal.Enabled = true;
+        hit.Enabled = true;
+        pass.Enabled = true;
+    }
+}
